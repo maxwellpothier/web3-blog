b4d08ddf42395936bddcde64d080c93dac9105af
maxwellpothier <pothiermaxwell@gmail.com>
2022-11-15 22:16:13 -0700

    finishes tests

--- test/Blog.js
+++ test/Blog.js
@@ -23,6 +23,53 @@ describe("Blog", () => {
 			const actualName = await helloWorld.name();
 
 			assert(expectedName === actualName, "Names do not match");
+
+			await helloWorld.updateName("Updated name");
+			const expectedSecondName = "Updated name";
+			const actualSecondName = await helloWorld.name();
+
+			assert(expectedSecondName === actualSecondName, "Update name failed");
+		});
+		it("successfully transfers ownership", async () => {
+			const {helloWorld, owner, otherAddress} = await loadFixture(deployBlogFixture);
+
+			const currOwner = await helloWorld.owner();
+			assert(currOwner === owner.address);
+
+			await helloWorld.transferOwnership(otherAddress.address);
+			assert(await helloWorld.owner() === otherAddress.address);
+		});
+		it("fails to transfer ownership", async () => {
+			const {helloWorld, otherAddress} = await loadFixture(deployBlogFixture);
+
+			await expect(helloWorld.connect(otherAddress).transferOwnership(otherAddress.address)).to.be.reverted;
+		});
+	});
+
+	describe("Creates a post", () => {
+		it("successfully creates a post", async () => {
+			const {helloWorld} = await loadFixture(deployBlogFixture);
+
+			await expect(helloWorld.createPost("My first post", "1234")).to.emit(helloWorld, "PostCreated");
+
+			const post = await helloWorld.fetchPost("1234");
+
+			assert(post.title === "My first post");
+		});
+	});
+
+	describe("Updates existing psot", () => {
+		it("successfully updates post", async () => {
+			const {helloWorld} = await loadFixture(deployBlogFixture);
+
+			await helloWorld.createPost("first post", "1234");
+			await helloWorld.createPost("second post", "5678");
+
+			await expect(helloWorld.updatePost(2, "updated second post", "5678", false)).to.emit(helloWorld, "PostUpdated");
+
+			const updatedPost = await helloWorld.fetchPost("5678");
+
+			assert(updatedPost.title === "updated second post");
 		});
 	});
 });
