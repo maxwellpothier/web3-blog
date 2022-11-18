982c8e541fbcd1752748795b01b8242a9bb93d4f
maxwellpothier <pothiermaxwell@gmail.com>
2022-11-17 19:48:08 -0700

    configures deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,16 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.17",
+	solidity: "0.8.17",
+	networks: {
+		goerli: {
+			url: process.env.RPC_URL,
+			accounts: [process.env.WALLET_PRIVATE_KEY],
+		},
+		hardhat: {
+			chainId: 1337,
+		},
+	},
 };
